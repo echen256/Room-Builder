80dd69d091ff7b63b4b65466914d2b2b1bd8a13e
echen256 <ericthechen@gmail.com>
2020-01-05 19:38:36 -0800

    f

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -4,13 +4,14 @@ const cors = require('cors');
 const fs = require(
     "fs"
 )
+const proxy = require("http-proxy-middleware");
 
-var https = require('https');
 /* Init express */
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors())
+app.use(cors());
+// app.use(proxy["/"], {target : "http://localhost:3001"})
 app.get("/load", function (req, res) {
     var file = fs.readFileSync(__dirname + "/Data/data.json", "utf8");
     res.send(file);
